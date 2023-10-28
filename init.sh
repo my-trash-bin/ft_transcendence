0b500b782b9b7fa1c795dcd7f8e7ff79811ae56a
맹주영 <mjy9088@naver.com>
2023-10-28 14:47:27 +0900

    WIP: update schema, settings

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd "$(dirname "$0")"
-
-find setting/workspace -type f | cut -c 19- | sed s/\\.txt$// | while IFS= read -r WORKSPACE_NAME; do
-  < "setting/workspace/$WORKSPACE_NAME.txt" sed s=^=setting/include/= | sed s/$/.txt/ | xargs cat > "$WORKSPACE_NAME.code-workspace"
-done
