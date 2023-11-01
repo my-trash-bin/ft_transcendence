bd7df55ac50ab94bcef731a9eb0c79ad201254bb
christie26 <yoonseo9800@gmail.com>
2023-11-01 16:29:16 +0900

    [feat] only flow & create link-button

--- project/frontend/src/app/page.tsx
+++ project/frontend/src/app/page.tsx
@@ -1,5 +1,11 @@
-import FriendHome from './friend/page';
+import LinkButton from '../../components/common/button/link-button';
 
 export default function Page() {
-  return <FriendHome />;
+  return (
+    <>
+      <h1>Here is starting page</h1>
+      <LinkButton text="go to friend" href="/friend" />
+      <LinkButton text="go to login page" href="/login" />
+    </>
+  );
 }
