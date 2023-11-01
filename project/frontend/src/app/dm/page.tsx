342499739fb8c5295d11724beb1ee4e918d9b792
wken5577 <wken5577@hanmail.net>
2023-11-01 15:13:26 +0900

    [Feat] message box layout 잡는중 (#2)

--- project/frontend/src/app/dm/page.tsx
+++ project/frontend/src/app/dm/page.tsx
@@ -1,5 +1,6 @@
 import { DmUserList } from '../../../components/channel/dm-user/DmUserList';
 import { MessageSearch } from '../../../components/channel/message-search/MessageSearch';
+import { UserInfo } from '../../../components/channel/message/UserInfo';
 
 export default function dmHome() {
   return (
@@ -8,11 +9,19 @@ export default function dmHome() {
           <Navbar />
         </nav> */}
 
-      <div className={`${'layout.dm-left'}`}>
-        <MessageSearch />
-        <DmUserList />
+      <div className="flex flex-row">
+        <div className="w-5/12 h-full">
+          <MessageSearch />
+          <DmUserList />
+        </div>
+        <div className="w-7/12 h-full">
+          <UserInfo
+            imageUri="/images/avataaars.svg"
+            username="user1"
+            onActive={true}
+          />
+        </div>
       </div>
-      <div className={`${'layout.dm-right'}`}></div>
     </>
   );
 }
