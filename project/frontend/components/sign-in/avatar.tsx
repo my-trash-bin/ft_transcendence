a5ac55c358277720608d615ae8517589e95e3cc4
christie26 <yoonseo9800@gmail.com>
2023-11-02 23:50:13 +0900

    [feat] making sign-in process #4

---
+++ project/frontend/components/sign-in/avatar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import styles from './avatar.module.css';
+
+type AvatarProps = {
+  name: string;
+  isSelected: boolean;
+};
+
+export default function Avatar({ name, isSelected }: AvatarProps) {
+  const src = `/avatar/${name}`;
+  const alt = { name };
+
+  const avatarClassName = isSelected ? styles.active_card : styles.card;
+
+  return (
+    <Image
+      src={src}
+      priority={true}
+      alt="avatar"
+      width={100}
+      height={100}
+      className={avatarClassName}
+    />
+  );
+}
---
+++ project/frontend/components/sign-in/chooseNickname.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+type ChooseNicknameProps = {
+  onNicknameSubmit: (nickname: string) => void;
+};
+
+export default function ChooseNickname({
+  onNicknameSubmit,
+}: ChooseNicknameProps) {
+  const [nickname, setNickname] = useState('');
+
+  const handleNicknameSubmit = () => {
+    onNicknameSubmit(nickname);
+  };
+
+  return (
+    <div>
+      <h2>Step 1: Choose a Nickname</h2>
+      <input
+        type="text"
+        placeholder="Enter your nickname"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <button onClick={handleNicknameSubmit}>Confirm</button>
+    </div>
+  );
+}
