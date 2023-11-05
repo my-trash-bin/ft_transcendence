f6a9b03e2b7c73ba136a6f6ba32bdbfc6fc33a35
christie26 <yoonseo9800@gmail.com>
2023-11-05 15:40:22 +0900

    remove style/Home.module.css

--- project/frontend/src/app/game/page.tsx
+++ project/frontend/src/app/game/page.tsx
@@ -1,13 +1,12 @@
 'use client';
-import styles from '../../../styles/Home.module.css';
-import Navbar from '../../components/common/navbar/navbar';
+import Navbar from '../../components/common/navbar';
 // import GameButton from '../../components/game/Gamebutton.js';
-import GameButton from '../../components/game/Gamebutton.tsx';
+import GameButton from '../../components/game/GameButton';
 import pageStyles from './page.module.css';
 
 export default function FriendHome() {
   return (
-    <nav className={styles.layout}>
+    <nav className="flex">
       <Navbar />
       <div className={pageStyles.buttonContainer}>
         <GameButton mode="normal" />
