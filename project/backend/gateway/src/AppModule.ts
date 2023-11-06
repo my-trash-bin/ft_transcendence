ce4f118f505b857c1035567aa001a8e4d644efc3
Juyeong Maing <mjy9088@naver.com>
2023-11-07 00:48:03 +0900

    add authentication header

--- project/backend/gateway/src/AppModule.ts
+++ project/backend/gateway/src/AppModule.ts
@@ -1,4 +1,4 @@
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { env } from '@ft_transcendence/common/env';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
@@ -11,6 +11,17 @@ import { sign } from 'jsonwebtoken';
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       gateway: {
+        buildService: ({ name, url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }: any) {
+              request.http.headers.set(
+                'Authorization',
+                `Bearer ${context.token}`,
+              );
+            },
+          });
+        },
         supergraphSdl: new IntrospectAndCompose({
           subgraphs: [
             { name: 'auth', url: env('SUBGRAPH_URL_AUTH') },
