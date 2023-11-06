a365fd692e8377310bfb16605ef7a52b8771dffc
wken5577 <wken5577@hanmail.net>
2023-11-06 17:10:27 +0900

    [Feat] channel search, select, add 완료 (#16)

--- project/frontend/src/app/channel/page.tsx
+++ project/frontend/src/app/channel/page.tsx
@@ -1,10 +1,20 @@
+import { SelectChannel } from '@/components/channel/SelectChannel';
+import { MessageSearchInput } from '@/components/channel/message-search/MessageSearchInput';
 import Navbar from '../../components/common/navbar';
 
-export default function FriendHome() {
+export default function ChannelHome() {
   return (
-    <nav className="flex">
+    <div className="flex flex-row">
       <Navbar />
-      <div> This is channel page </div>
-    </nav>
+      <div className="flex flex-col h-[inherit] w-[1280px] mt-0 mb-0 ml-auto mr-auto">
+        <div className="ml-[30px] w-[350px]">
+          <h3 className="w-[270px] h-[50px] text-[32px] mt-[30px] mb-[10px]">
+            채널
+          </h3>
+          <SelectChannel myChannel={true} />
+          <MessageSearchInput />
+        </div>
+      </div>
+    </div>
   );
 }
