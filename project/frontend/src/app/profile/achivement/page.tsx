f979dd18028d82b234de9e90f34caf363c992594
christie26 <yoonseo9800@gmail.com>
2023-11-07 15:08:24 +0900

    add pages for detail-profile

---
+++ project/frontend/src/app/profile/achivement/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import Navbar from '../../../components/common/Navbar';
+import ArchivementArticle from '../../../components/profile/ArchivementArticle';
+
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-row w-[100%] h-[100%]">
+      <Navbar />
+      <div className="w-[100%]">
+        <div className="flex flex-col items-center">
+          <ArchivementArticle />
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+}
