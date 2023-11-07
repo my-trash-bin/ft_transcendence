0ad1f372e1e08b53e2c818bf13dc63eac8c92ea8
democracyKim <107744617+juspian@users.noreply.github.com>
2023-11-07 17:09:35 +0900

    [feat] remove css file

--- project/frontend/src/components/game/ModalMatch.tsx
+++ project/frontend/src/components/game/ModalMatch.tsx
@@ -1,42 +1,43 @@
-import PropTypes from 'prop-types';
+import React from 'react';
 import Modal from 'react-modal';
-import './ModalMatch.css';
 
-const MatchingModal = ({ isOpen, onClose, mode }) => {
+interface MatchingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  mode: 'normal' | 'item';
+}
+
+const MatchingModal: React.FC<MatchingModalProps> = ({ isOpen, onClose, mode }) => {
   if (!isOpen) {
     return null;
   }
+  const bgCSS = 'bg-default rounded-md';
+  const size = 'py-sm px-lg w-[498px] h-[308px]';
+  const borderCSS = 'border-dark-purple border-4';
+  const textCSS = 'text-dark-purple text-h2';
+  const alignCSS = 'items-center justify-center';
+  const positionCSS = 'mx-auto mt-[200px]';
 
-  const customStyles = {
-    content: {
-      width: '50vw',
-      height: '35vw',
-      top: '35vw',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      border: 'none',
-      backgroundColor: 'transparent',
-    },
-  };
+  const hoverCSS = 'cursor-pointer';
+  const txtPos = 'text-center mt-[80px]';
+  const buttonCSS = 'bg-dark-purple-interactive rounded-md \
+  text-light-gray-interactive text-h2 \
+  w-lg mt-[65pt] ml-[250pt]';
+
+  let content = mode === 'normal' ? '일반' : '아이템';
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
-      <div className="matching-modal-content">
-        <div className="text-center">
-          <p>{mode} 매칭이 진행중입니다.</p>
+    <Modal isOpen={isOpen} onRequestClose={onClose} shouldCloseOnOverlayClick={false} className="bg-transparent">
+      <div className={`${textCSS} ${alignCSS} ${positionCSS} ${borderCSS} ${size} ${bgCSS}`}>
+        <div className={`${txtPos}`}>
+          <p>{content} 매칭이 진행중입니다.</p>
         </div>
-        <button className="matching-modal-close-button" onClick={onClose}>
+        <button className={`${buttonCSS} ${hoverCSS}`} onClick={onClose}>
           닫기
         </button>
       </div>
     </Modal>
   );
-};
-
-MatchingModal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-  mode: PropTypes.oneOf(['normal', 'item']).isRequired,
 };
 
 export default MatchingModal;
