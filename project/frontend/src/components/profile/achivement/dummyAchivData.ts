8fbdd212eef3bd2fdf54c68368132b13c6e5d34d
christie26 <yoonseo9800@gmail.com>
2023-11-07 17:09:33 +0900

    make badge component

---
+++ project/frontend/src/components/profile/achivement/dummyAchivData.ts
@@ -0,0 +1,45 @@
+interface AchivementProps {
+  key: number;
+  imageURL: string;
+  name: string;
+  explanation: string;
+}
+
+export const dummyAchivData: AchivementProps[] = [
+  {
+    key: 1,
+    imageURL: '/avatar/avatar-black.svg',
+    name: '출석왕',
+    explanation: '이번달 5회 출석 달성!',
+  },
+  {
+    key: 2,
+    imageURL: '/avatar/avatar-black.svg',
+    name: '게임왕',
+    explanation: '이번주 5회 개임 달성!',
+  },
+  {
+    key: 3,
+    imageURL: '/avatar/avatar-black.svg',
+    name: '짱친',
+    explanation: '접속 5회 달성!',
+  },
+  {
+    key: 4,
+    imageURL: '/avatar/avatar-black.svg',
+    name: 'Achievement 4',
+    explanation: 'This is the first achievement.',
+  },
+  {
+    key: 5,
+    imageURL: '/avatar/avatar-black.svg',
+    name: 'Achievement 5',
+    explanation: 'This is the second achievement.',
+  },
+  {
+    key: 6,
+    imageURL: '/avatar/avatar-black.svg',
+    name: 'Achievement 6',
+    explanation: 'This is the third achievement.',
+  },
+];
