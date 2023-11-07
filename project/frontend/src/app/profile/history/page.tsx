8b3d8f8d22deaea95d9feafc0c9c0cc2e231c81d
christie26 <yoonseo9800@gmail.com>
2023-11-07 18:32:21 +0900

    achive, history with mockData

--- project/frontend/src/app/profile/history/page.tsx
+++ project/frontend/src/app/profile/history/page.tsx
@@ -1,25 +1,14 @@
 'use client';
 import Navbar from '../../../components/common/Navbar';
-import ArchivementArticle from '../../../components/profile/achivement/ArchivementArticle';
 import HistoryArticle from '../../../components/profile/history/HistoryArticle';
-import ProfileArticle from '../../../components/profile/ProfileArticle';
 
-export default function ProfilePage() {
+export default function HistoryPage() {
   return (
     <div className="flex flex-row w-[100%] h-[100%]">
       <Navbar />
       <div className="w-[100%]">
         <div className="flex flex-col items-center">
-          <ProfileArticle
-            avatar="/avatar/avatar-blue.svg"
-            nickname="happy"
-            win={3}
-            lose={5}
-            ratio={37}
-            statusMessage={'I can do it!'}
-          />
           <div className="flex flex-row">
-            <ArchivementArticle />
             <HistoryArticle />
           </div>
         </div>
--- project/frontend/src/components/profile/achivement/ArchivementArticle.tsx
+++ project/frontend/src/components/profile/achivement/ArchivementArticle.tsx
@@ -1,19 +1,31 @@
 import Link from 'next/link';
+import { Badge } from './Badge';
+import { mockData } from './mockData';
 
 function ArchivementArticle() {
   const buttonClass =
     'w-lg h-sm bg-default rounded-sm border-2 border-dark-purple ' +
     'text-center text-black text-lg font-bold hover:bg-light-background  ' +
     'flex items-center justify-center ' +
-    'absolute top-xl right-xl';
+    'absolute top-xl right-2xl';
   return (
-    <div className="w-[600px] h-xl bg-light-background rounded-lg mt-xl ml-xl relative">
+    <div className="w-[800px] h-2xl bg-light-background rounded-lg mt-xl ml-xl relative">
       <div className="h-[inherit] pt-3xl flex flex-col items-center">
-        <h2 className="text-h2 font-bold absolute top-xl left-xl">업적</h2>
-        <Link href="/profile/achivement" className={buttonClass}>
-          더보기
+        <h2 className="text-h2 font-bold absolute top-[40px] left-2xl">업적</h2>
+        <Link href="/profile" className={buttonClass}>
+          돌아가기
         </Link>
-        <p>archivement information goes here.</p>
+
+        <div className="grid grid-cols-3 pt-2xl gap-xl">
+          {mockData.map((badge) => (
+            <Badge
+              key={badge.key}
+              imageURL={badge.imageURL}
+              name={badge.name}
+              explanation={badge.explanation}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
