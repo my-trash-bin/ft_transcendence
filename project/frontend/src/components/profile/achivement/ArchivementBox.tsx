00e486aa001f99daf72660cb560ea2460e4d0e76
christie26 <yoonseo9800@gmail.com>
2023-11-08 15:01:32 +0900

    make ProfileButton

--- project/frontend/src/components/profile/achivement/ArchivementBox.tsx
+++ project/frontend/src/components/profile/achivement/ArchivementBox.tsx
@@ -1,18 +1,13 @@
-import Link from 'next/link';
+import { ProfileButton } from '../ProfileButton';
 
 function ArchivementBox() {
-  const buttonClass =
-    'w-lg h-sm bg-default rounded-sm border-2 border-dark-purple ' +
-    'text-center text-black text-lg font-bold hover:bg-light-background  ' +
-    'flex items-center justify-center ' +
-    'absolute top-xl right-xl';
   return (
     <div className="w-[435px] h-[420px] bg-light-background rounded-lg relative">
       <div className="h-[inherit] pt-3xl flex flex-col items-center">
-        <h2 className="text-h2 font-bold absolute top-xl left-xl">업적</h2>
-        <Link href="/profile/achivement" className={buttonClass}>
-          더보기
-        </Link>
+        <h2 className="text-h2 font-semibold text-dark-gray absolute top-xl left-xl">
+          업적
+        </h2>
+        <ProfileButton href="/profile/achivement" text="더보기" />
         <p>archivement information goes here.</p>
       </div>
     </div>
--- project/frontend/src/components/profile/history/HistoryBox.tsx
+++ project/frontend/src/components/profile/history/HistoryBox.tsx
@@ -1,19 +1,14 @@
-import Link from 'next/link';
+import { ProfileButton } from '../ProfileButton';
 
 function HistoryBox() {
-  const buttonClass =
-    'w-lg h-sm bg-default rounded-sm border-2 border-dark-purple ' +
-    'text-center text-black text-lg font-bold hover:bg-light-background  ' +
-    'flex items-center justify-center ' +
-    'absolute top-xl right-xl';
   return (
     <div className="w-[435px] h-[420px] bg-light-background rounded-lg ml-[30px] relative">
       <div className="h-[inherit] pt-3xl flex flex-col items-center">
-        <h2 className="text-h2 font-bold absolute top-xl left-xl">최근 전적</h2>
+        <h2 className="text-h2 font-semibold text-dark-gray absolute top-xl left-xl">
+          최근 전적
+        </h2>
         <p>history information goes here.</p>
-        <Link href="/profile/history" className={buttonClass}>
-          더보기
-        </Link>
+        <ProfileButton href="/profile/history" text="더보기" />
       </div>
     </div>
   );
