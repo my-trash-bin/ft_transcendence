09a9f9a86b717b293c351a5b0ba49c2fc0ce4dc7
christie26 <yoonseo9800@gmail.com>
2023-11-09 13:49:50 +0900

    Title cva component

--- project/frontend/src/components/profile/achivement/ArchivementArticle.tsx
+++ project/frontend/src/components/profile/achivement/ArchivementArticle.tsx
@@ -1,19 +1,13 @@
+import { Title } from '@/components/common/Title';
 import { ProfileButton } from '../ProfileButton';
 import { Badge } from './Badge';
 import { mockData } from './mockData';
 
 function ArchivementArticle() {
-  const buttonClass =
-    'w-lg h-sm bg-default rounded-sm border-2 border-dark-purple ' +
-    'text-center text-black text-lg font-bold hover:bg-light-background  ' +
-    'flex items-center justify-center ' +
-    'absolute top-xl right-2xl';
   return (
     <div className="relative">
       <div className="h-[inherit] pt-3xl flex flex-col items-center">
-        <h2 className="text-h2 text-dark-gray font-bold absolute top-2xl left-1/2 transform -translate-x-1/2">
-          최근 전적
-        </h2>
+        <Title location="top-center">업적</Title>
         <ProfileButton href="/profile" text="돌아가기" />
         <div className="grid grid-cols-3 h-[500px] mt-3xl gap-xl overflow-y-scroll">
           {mockData.map((badge) => (
