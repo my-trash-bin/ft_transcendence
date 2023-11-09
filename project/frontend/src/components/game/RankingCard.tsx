318a0cc164714f549daaf097953ab3945b7078a7
christie26 <yoonseo9800@gmail.com>
2023-11-09 12:02:54 +0900

    apply cva to Ranking

--- project/frontend/src/components/game/RankingCard.tsx
+++ project/frontend/src/components/game/RankingCard.tsx
@@ -1,30 +1,30 @@
 import Image from 'next/image';
-import LongCard from '../common/LongCard';
+import { LongCard } from '../common/LongCard';
 
 interface RankingCardProps {
-  item: {
-    rank: number;
-    name: string;
-    avatar: string;
-  };
+  rank: number;
+  name: string;
+  avatar: string;
   isUser: boolean;
 }
 
-export default function RankingCard({ item, isUser }: RankingCardProps) {
+export default function RankingCard({
+  rank,
+  name,
+  avatar,
+  isUser,
+}: RankingCardProps) {
   const alignCSS = 'flex items-center justify-between w-[600px]';
-
   return (
-    <LongCard isUser={isUser}>
+    <LongCard
+      size={isUser ? 'big' : 'medium'}
+      color={isUser ? 'color' : 'default'}
+    >
       <div className={`${alignCSS}`}>
         <span>
-          {item.rank}. {item.name}
+          {rank}. {name}
         </span>
-        <Image
-          src={item.avatar}
-          alt={`${item.name}'s avatar`}
-          width={50}
-          height={50}
-        />
+        <Image src={avatar} alt={`${name}'s avatar`} width={50} height={50} />
       </div>
     </LongCard>
   );
