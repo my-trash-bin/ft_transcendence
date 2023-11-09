f143f217bf7ee909603c98463f9ba97735c8b9af
christie26 <yoonseo9800@gmail.com>
2023-11-09 16:22:40 +0900

    friend page - CommonCard

---
+++ project/frontend/src/components/friend/utils/mockFriends.ts
@@ -0,0 +1,32 @@
+export const mockFriends = [
+  {
+    key: '1',
+    imageURL: '/avatar/avatar-big.svg',
+    nickname: 'klew',
+  },
+  {
+    key: '2',
+    imageURL: '/avatar/avatar-small.svg',
+    nickname: 'minkim',
+  },
+  {
+    key: '3',
+    imageURL: '/avatar/avatar-black.svg',
+    nickname: 'yoonsele',
+  },
+  {
+    key: '4',
+    imageURL: '/avatar/avatar-small.svg',
+    nickname: 'hyunn',
+  },
+  {
+    key: '5',
+    imageURL: '/avatar/avatar-big.svg',
+    nickname: 'klew',
+  },
+  {
+    key: '6',
+    imageURL: '/avatar/avatar-small.svg',
+    nickname: 'kyu',
+  },
+];
