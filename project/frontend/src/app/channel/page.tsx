67ce6d74fd0791b8b9ec3d6b73e503da27c890b4
wken5577 <wken5577@hanmail.net>
2023-11-15 13:37:37 +0900

    [Feat] channel list fetch with gql (#29)

--- project/frontend/src/app/channel/page.tsx
+++ project/frontend/src/app/channel/page.tsx
@@ -3,18 +3,21 @@ import { ChannelInput } from '@/components/channel/ChannelInput';
 import { ChannelList } from '@/components/channel/ChannelList';
 import { ChannleMessageBox } from '@/components/channel/ChannelMessageBox';
 import { ModalLayout } from '@/components/channel/modals/ModalLayout';
+import { getClient } from '@/lib/ApolloClient';
+import { ApolloProvider } from '@apollo/client';
 import { useState } from 'react';
 export default function ChannelHome() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [myChannel, setMyChannel] = useState(true);
+  const [searchChannel, setSearchChannel] = useState('');
   const modalOpen = () => {
     setIsOpen(true);
   };
   const modalClose = () => {
     setIsOpen(false);
   };
-
   return (
-    <>
+    <ApolloProvider client={getClient()}>
       <ModalLayout
         isOpen={isOpen}
         closeModal={modalClose}
@@ -25,16 +28,20 @@ export default function ChannelHome() {
         className={`bg-opacity-50 flex flex-row bg-light-background rounded-[20px] w-[inherit]`}
       >
         <div className="w-[380px] h-[750px] border-r">
-          <ChannelInput />
+          <ChannelInput
+            myChannel={myChannel}
+            setMyChannel={setMyChannel}
+            setSearchChannel={setSearchChannel}
+          />
           <p className="text-[15px] text-dark-gray pl-[15px] mt-[15px] mb-[15px]">
-            내 채널
+            {myChannel ? '내 채널' : '모든 채널'}
           </p>
-          <ChannelList />
+          <ChannelList myChannel={myChannel} searchChannel={searchChannel} />
         </div>
         <div className="w-[520px] h-[750px] flex flex-col items-center">
           <ChannleMessageBox modalOpen={modalOpen} />
         </div>
       </div>
-    </>
+    </ApolloProvider>
   );
 }
