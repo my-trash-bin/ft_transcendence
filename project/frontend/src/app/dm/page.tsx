27ebe9874719682f71ff0a49791e796b29678445
hyunkyu <azdlgusrb@naver.com>
2023-11-16 13:29:24 +0900

    [Feat] useQuery update (#29)

--- project/frontend/src/app/dm/page.tsx
+++ project/frontend/src/app/dm/page.tsx
@@ -2,6 +2,8 @@
 
 import { DmUserList } from '@/components/dm/dm-user/DmUserList';
 import { MessageSearch } from '@/components/dm/message-search/MessageSearch';
+import { getClient } from '@/lib/ApolloClient';
+import { ApolloProvider } from '@apollo/client';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -12,23 +14,25 @@ export default function DmPage() {
   const [searchUsername, setSearchUsername] = useState('');
 
   return (
-    <div className="flex flex-row bg-light-background rounded-[20px] w-[inherit]">
-      <div className="w-[380px] h-[750px] border-r flex flex-col items-center">
-        <MessageSearch userSearchCallback={userSearchCallback} />
-        <p className="text-[15px] text-dark-gray mb-[15px] pl-[15px] self-start">
-          모든 메세지
-        </p>
-        <DmUserList searchUsername={searchUsername} />
+    <ApolloProvider client={getClient()}>
+      <div className="flex flex-row bg-light-background rounded-[20px] w-[inherit]">
+        <div className="w-[380px] h-[750px] border-r flex flex-col items-center">
+          <MessageSearch userSearchCallback={userSearchCallback} />
+          <p className="text-[15px] text-dark-gray mb-[15px] pl-[15px] self-start">
+            모든 메세지
+          </p>
+          <DmUserList searchUsername={searchUsername} />
+        </div>
+        <div className="w-[520px] h-[750px] flex flex-col items-center justify-center">
+          <Image
+            alt="dm image"
+            src="/images/dm-page.png"
+            priority={true}
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
-      <div className="w-[520px] h-[750px] flex flex-col items-center justify-center">
-        <Image
-          alt="dm image"
-          src="/images/dm-page.png"
-          priority={true}
-          width={300}
-          height={300}
-        />
-      </div>
-    </div>
+    </ApolloProvider>
   );
 }
--- project/frontend/src/components/channel/ChannelList.tsx
+++ project/frontend/src/components/channel/ChannelList.tsx
@@ -54,7 +54,6 @@ export function ChannelList({
               />
             );
           })}
-      ;
     </div>
   );
 }
