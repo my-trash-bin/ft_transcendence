ced4245456673514ad6df9d861695196bafa0c3e
wken5577 <wken5577@hanmail.net>
2023-11-17 11:59:28 +0900

    fix ParticipantCard css

--- project/frontend/src/components/channel/modals/ParticipantCard.tsx
+++ project/frontend/src/components/channel/modals/ParticipantCard.tsx
@@ -14,7 +14,7 @@ export function ParticipantCard({
   const button = type === 'ADD' ? <InviteButton /> : <SelectPenalty />;
 
   return (
-    <div className="bg-white mb-[10px] pl-[5px] w-[inherit] h-[70px] border border-default rounded-[10px] flex flex-row justify-between">
+    <div className="bg-white mb-[10px] pl-[10px] w-[280px] h-[70px] border border-default rounded-[10px] flex flex-row justify-between">
       <div className="flex flex-row items-center">
         <Image alt="user avatar" src={imageUrl} width={30} height={30}></Image>
         <p className="ml-[10px]">{nickname}</p>
