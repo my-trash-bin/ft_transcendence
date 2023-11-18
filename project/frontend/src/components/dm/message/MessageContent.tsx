3993d7813403e3f32bce0cd3b319886020f3e217
christie26 <yoonseo9800@gmail.com>
2023-11-18 15:16:39 +0900

    messagebox margin

--- project/frontend/src/components/dm/message/MessageContent.tsx
+++ project/frontend/src/components/dm/message/MessageContent.tsx
@@ -3,7 +3,7 @@ import { OtherChat } from './OtherChat';
 
 export function MessageContent() {
   return (
-    <div className="w-[95%] h-[610px] pt-[20px] bg-chat-color2 rounded-[10px] flex flex-col overflow-y-scroll">
+    <div className="w-[95%] h-[610px] pt-[20px] bg-chat-color2 rounded-[10px] flex flex-col overflow-y-scroll mt-sm">
       <MyChat content="sdassdfljsdaf" time={new Date()} />
       <MyChat
         content="sdassdfljsdafsdassdfljsdafsdassdfljsdaf"
