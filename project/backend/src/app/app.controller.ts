b2acbe973817d79cc0e82a2e69e440b21dbefd59
맹주영 <mjy9088@naver.com>
2023-11-18 21:21:29 +0900

    add 2FA, separate nickname register process

--- project/backend/src/app/app.controller.ts
+++ project/backend/src/app/app.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 import { Request as ExpressRequest } from 'express';
-import { AuthService } from '../auth/auth.service';
+import { AuthService, JwtPayloadPhaseComplete } from '../auth/auth.service';
 import { JwtGuard } from '../auth/jwt.guard';
+import { Phase, PhaseGuard } from '../auth/phase.guard';
 import { UserId } from '../common/Id';
 
 class MeResult {
@@ -19,12 +20,13 @@ export class AppController {
   constructor(private authService: AuthService) {}
 
   // TODO: apply https://docs.nestjs.com/openapi/operations
-  @UseGuards(JwtGuard)
+  @UseGuards(JwtGuard, PhaseGuard)
+  @Phase('complete')
   @Get('me')
   @ApiOkResponse({
     type: MeResult,
   })
   getProfile(@Request() req: ExpressRequest) {
-    return new MeResult(req.user!.userId);
+    return new MeResult((req.user as JwtPayloadPhaseComplete).id);
   }
 }
--- project/backend/src/auth/auth.service.ts
+++ project/backend/src/auth/auth.service.ts
@@ -1,34 +1,135 @@
+import { scrypt } from 'node:crypto';
+
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { v4 } from 'uuid';
+import { ConfigService } from '@nestjs/config';
+import { AuthType } from '@prisma/client';
 
 import { PrismaService } from '../base/prisma.service';
 import { UserId, idOf } from '../common/Id';
 
-export interface JwtPayload {
-  userId: UserId;
+// After OAuth, if user not exists
+export interface JwtPayloadPhaseRegister {
+  phase: 'register';
+  type: AuthType;
+  id: string;
 }
+
+// After OAuth, if user exists and 2fa set
+export interface JwtPayloadPhase2FA {
+  phase: '2fa';
+  type: AuthType;
+  id: string;
+}
+
+export interface JwtPayloadPhaseComplete {
+  phase: 'complete';
+  id: UserId;
+}
+
+export type JwtPayload =
+  | JwtPayloadPhaseRegister
+  | JwtPayloadPhase2FA
+  | JwtPayloadPhaseComplete;
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly jwtService: JwtService,
     private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
   ) {}
 
-  async login(user: any): Promise<string> {
-    const metadataJson = JSON.stringify(user._json);
-    const prismaAuth = await this.prismaService.auth.upsert({
-      where: { type_id: { type: 'FT', id: user.id } },
-      create: {
-        type: 'FT',
-        id: user.id,
-        metadataJson,
-        user: { create: { nickname: `new user ${v4()}` } },
+  async oauth42(ftUser: any): Promise<JwtPayload> {
+    const metadataJson = ftUser._raw;
+    return await this.prismaService.$transaction(
+      async (tx): Promise<JwtPayload> => {
+        const auth = await tx.auth.findUnique({
+          where: { type_id: { type: 'FT', id: ftUser.id } },
+          select: { userId: true },
+        });
+        if (!auth) {
+          const { type, id } = await tx.auth.create({
+            data: { type: 'FT', id: ftUser.id, metadataJson },
+            select: { type: true, id: true },
+          });
+          return { phase: 'register', type, id };
+        } else {
+          const { type, id, user } = await tx.auth.update({
+            where: { type_id: { type: 'FT', id: ftUser.id } },
+            data: { metadataJson },
+            select: {
+              type: true,
+              id: true,
+              user: { select: { id: true, mfaPasswordHash: true } },
+            },
+          });
+          if (user?.mfaPasswordHash) {
+            return { phase: '2fa', type, id };
+          } else if (!user) {
+            return { phase: 'register', type, id };
+          } else {
+            return { phase: 'complete', id: idOf(user.id) };
+          }
+        }
       },
-      update: { metadataJson },
+    );
+  }
+
+  async register(
+    authType: AuthType,
+    authId: string,
+    nickname: string,
+    password?: string,
+  ): Promise<JwtPayload> {
+    return await this.prismaService.$transaction(
+      async (tx): Promise<JwtPayload> => {
+        const auth = await tx.auth.update({
+          where: { type_id: { type: authType, id: authId } },
+          data: {
+            user: {
+              create: {
+                nickname,
+                mfaPasswordHash:
+                  password && (await this.mfaPasswordHash(password)),
+              },
+            },
+          },
+          select: { user: { select: { id: true } } },
+        });
+        const { id } = auth!.user!;
+        return { phase: 'complete', id: idOf(id) };
+      },
+    );
+  }
+
+  async twoFactorAuthentication(
+    authType: AuthType,
+    authId: string,
+    password: string,
+  ): Promise<JwtPayload | undefined> {
+    const auth = await this.prismaService.auth.findUnique({
+      where: { type_id: { type: authType, id: authId } },
+      select: { user: { select: { id: true, mfaPasswordHash: true } } },
     });
-    const payload: JwtPayload = { userId: idOf(prismaAuth.userId) };
-    return this.jwtService.sign(payload);
+    if (
+      (await this.mfaPasswordHash(password)) === auth!.user!.mfaPasswordHash
+    ) {
+      return { phase: 'complete', id: idOf(auth!.user!.id) };
+    } else {
+      return undefined;
+    }
+  }
+
+  private mfaPasswordHash(password: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      scrypt(
+        password,
+        this.configService.getOrThrow<string>('PASSWORD_SALT'),
+        32,
+        (err, buffer) => {
+          if (err) reject(err);
+          else resolve(buffer.toString());
+        },
+      );
+    });
   }
 }
