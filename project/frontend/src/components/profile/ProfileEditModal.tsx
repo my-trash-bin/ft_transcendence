31ccfcffe6a2f64d9465b2a741c4e2c5ed3bbd67
christie26 <yoonseo9800@gmail.com>
2023-11-25 16:49:51 +0900

    fix ProfileEditModal

--- project/frontend/src/components/profile/ProfileEditModal.tsx
+++ project/frontend/src/components/profile/ProfileEditModal.tsx
@@ -1,13 +1,14 @@
-import { UserDto } from '@/api/api';
+import { UserProfileDto } from '@/api/api';
 import Image from 'next/image';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import { ApiContext } from '../../app/_internal/provider/ApiContext';
 import { ModalLayout } from '../channel/modals/ModalLayout';
 
 interface ModalProfileProps {
   isOpen: boolean;
   onClose: () => void;
-  fetchData: () => Promise<void>;
+  fetchData: () => Promise<unknown>;
+  defaultData: UserProfileDto;
 }
 function isNicknameValid(nickname: string): boolean {
   const nicknameRegex = /^[a-zA-Z0-9\-_]{6,12}$/;
@@ -18,43 +19,24 @@ export const ProfileEditModal: React.FC<ModalProfileProps> = ({
   isOpen,
   onClose,
   fetchData,
+  defaultData,
 }) => {
-  const [isChanged, setIsChanged] = useState(false);
   const { api } = useContext(ApiContext);
-  const mockId: string = '172daa3c-10af-4b37-b4d5-7a2f4ccc0dc2';
-  // TODO: get id from cookie
-  const [profileData, setProfileData] = useState<UserDto>({
-    id: '',
-    nickname: '',
-    profileImageUrl: '',
-    joinedAt: '',
-    isLeaved: false,
-    leavedAt: undefined,
-    statusMessage: '',
-  });
+  const [isChanged, setIsChanged] = useState(false);
+  const [profileData, setProfileData] = useState<UserProfileDto>(defaultData);
 
-  const getProfileData = useCallback(async () => {
+  const saveChanges = useCallback(async () => {
     try {
-      const result = await api.usersControllerFindOne(mockId);
-      // TODO: -> /api/v1/users/profile GET
-      // TODO: me api -> replace mockId
-      if (result.ok) {
-        setProfileData(result.data);
-      } else {
-        console.error({ result });
-        alert('TODO: Handle the error gracefully');
-      }
+      await api.usersControllerUpdate({
+        nickname: profileData.nickname,
+      });
+      fetchData();
+      onClose();
     } catch (error) {
-      console.error('Error fetching profile data:', error);
-      alert('TODO: Handle the error gracefully');
+      console.error('Error saving changes:', error);
+      alert('// TODO: Handle errors gracefully');
     }
-  }, [api, mockId]);
-
-  useEffect(() => {
-    if (isOpen) {
-      getProfileData();
-    }
-  }, [isOpen, getProfileData]);
+  }, [api, , profileData.nickname, onClose, fetchData]);
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newNickname = e.target.value;
@@ -78,23 +60,6 @@ export const ProfileEditModal: React.FC<ModalProfileProps> = ({
     setIsChanged(true);
   };
 
-  const saveChanges = useCallback(async () => {
-    try {
-      const result = await api.usersControllerUpdate(mockId, {
-        nickname: profileData.nickname,
-      });
-      if (!result.ok) {
-        console.error({ result });
-        alert('// TODO: Handle errors gracefully');
-      }
-      fetchData();
-      onClose();
-    } catch (error) {
-      console.error('Error saving changes:', error);
-      alert('// TODO: Handle errors gracefully');
-    }
-  }, [api, mockId, profileData.nickname, onClose, fetchData]);
-
   const textClass = 'font-bold text-xl text-dark-purple leading-loose';
   const buttonClass =
     'w-[80px] h-[30px] rounded-sm border-2 text-center text-black text-lg font-bold hover:bg-light-background self-end';
@@ -111,9 +76,9 @@ export const ProfileEditModal: React.FC<ModalProfileProps> = ({
       <div className="w-[100%] h-[100%] relative">
         <div className="p-xl h-[100%] flex flex-col gap-lg justift-center items-center">
           <p className="text-h2 font-bold text-dark-purple">프로필 수정</p>
-          {profileData.profileImageUrl ? (
+          {defaultData.imageUrl ? (
             <Image
-              src={profileData.profileImageUrl}
+              src={defaultData.imageUrl}
               alt="avatar"
               width={150}
               height={150}
@@ -132,7 +97,7 @@ export const ProfileEditModal: React.FC<ModalProfileProps> = ({
               type="text"
               value={profileData.nickname}
               onChange={handleNicknameChange}
-              placeholder={profileData.nickname}
+              placeholder={defaultData.nickname}
               className="bg-[#f3f0f8] border-2 border-dark-purple-interactive w-[200px]"
             />
             <p className={textClass}>상태메세지</p>
@@ -140,12 +105,12 @@ export const ProfileEditModal: React.FC<ModalProfileProps> = ({
               type="text"
               value={profileData.statusMessage}
               onChange={handleStatusMessageChange}
-              placeholder={profileData.statusMessage}
+              placeholder={defaultData.statusMessage}
               className="bg-[#f3f0f8] border-2 border-dark-purple"
             />
           </div>
           <button
-            disabled={!isChanged || !isNicknameValid(profileData.nickname)}
+            disabled={!isChanged || !isNicknameValid(defaultData.nickname)}
             onClick={saveChanges}
             className={`${buttonClass} ${colorClass}`}
           >
