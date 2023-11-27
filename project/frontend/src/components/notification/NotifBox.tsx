8dccbdb999b088ba221fc2130dcbea501a82060c
christie26 <yoonseo9800@gmail.com>
2023-11-27 17:35:47 +0900

    split NotifBox

---
+++ project/frontend/src/components/notification/NotifBox.tsx
@@ -0,0 +1,42 @@
+import { NotiCard } from './NotiCard';
+import mockNotifications from './mockNoti';
+import { useEffect, useRef, useState } from 'react';
+
+export function NotifBox() {
+  const [active, setActive] = useState(false);
+  const boxRef = useRef<HTMLDivElement>(null);
+  const data = mockNotifications;
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (boxRef.current && !boxRef.current.contains(event.target)) {
+        setActive(false);
+      }
+      event.stopPropagation();
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  return (
+    active && (
+      <div
+        ref={boxRef}
+        className={
+          'w-[250px] h-[400px] fixed left-[100px] bottom-[50px] \
+        rounded-[5px] border-3 border-dark-purple bg-default \
+        felx flex-col items-center justify-center \
+        overflow-y-scroll'
+        }
+      >
+        {data.map((val) => (
+          <NotiCard
+            key={val.id}
+            isRead={val.isRead}
+            content={val.contentJson}
+          />
+        ))}
+      </div>
+    )
+  );
+}
--- project/frontend/src/components/notification/Notification.tsx
+++ project/frontend/src/components/notification/Notification.tsx
@@ -1,28 +1,16 @@
 'use client';
-import { NotiCard } from './NotiCard';
-import mockNotifications from './mockNoti';
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+import { io } from 'socket.io-client';
+import { NotifBox } from './NotifBox';
 
 export function Notification() {
   const [isHovered, setIsHovered] = useState(false);
   const [active, setActive] = useState(false);
-  const boxRef = useRef<HTMLDivElement>(null);
-  const data = mockNotifications;
   // TODO : call api to fetch noti table
 
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (boxRef.current && !boxRef.current.contains(event.target)) {
-        setActive(false);
-      }
-      event.stopPropagation();
-    }
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  const socket = io();
+  socket.on('newNotif', () => console.log('get socket signal'));
 
   return (
     <div
@@ -43,25 +31,7 @@ export function Notification() {
         onMouseLeave={() => setIsHovered(false)}
         onClick={() => setActive(!active)}
       />
-      {active && (
-        <div
-          ref={boxRef}
-          className={
-            'w-[250px] h-[400px] fixed left-[100px] bottom-[50px] \
-            rounded-[5px] border-3 border-dark-purple bg-default \
-            felx flex-col items-center justify-center \
-            overflow-y-scroll'
-          }
-        >
-          {data.map((val) => (
-            <NotiCard
-              key={val.id}
-              isRead={val.isRead}
-              content={val.contentJson}
-            />
-          ))}
-        </div>
-      )}
+      <NotifBox />
     </div>
   );
 }
