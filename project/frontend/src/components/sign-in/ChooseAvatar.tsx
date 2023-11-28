5712cc851132ca3c9bc969a51f2b8ff83332d51f
christie26 <yoonseo9800@gmail.com>
2023-11-28 19:55:01 +0900

    multipart... ing

--- project/frontend/src/components/sign-in/ChooseAvatar.tsx
+++ project/frontend/src/components/sign-in/ChooseAvatar.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 import { Button } from '../common/Button';
-import SelectAvatar from './SelectAvatar';
-
+import { SelectAvatar } from './SelectAvatar';
 type ChooseAvatarProps = {
   readonly avatars: string[];
   readonly onChooseClick: (avatar: string) => void;
@@ -13,22 +13,56 @@ export default function ChooseAvatar({
   avatars,
   onChooseClick,
 }: ChooseAvatarProps) {
-  const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
+  const [selectedAvatar, setSelectedAvatar] = useState<any>(null);
+  const [uploadImage, setUploadImage] = useState<any>(null);
+
+  const onChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target) {
+          setUploadImage(e.target.result);
+          setSelectedAvatar(e.target.result);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div className="w-2xl h-[400px] bg-light-background rounded-lg flex flex-col justify-center items-center">
       <h2 className="font-bold mb-lg">2. 사용하실 아바타를 선택하세요.</h2>
       <div className="flex flex-col">
         <div className="grid grid-rows-2 grid-flow-col gap-xl mb-xl">
-          {avatars.map((avatar, index) => (
+          {avatars.map((avatar) => (
             <SelectAvatar
-              key={index}
+              key={avatar}
               imageUrl={avatar}
               isSelected={selectedAvatar === avatar}
               onClick={() => setSelectedAvatar(avatar)}
             />
           ))}
-          <div className="w-lg h-lg flex items-center justify-center border-3 border-default hover:border-dark-gray hover:bg-light-background" />
+          <div className="border-3 border-dark-purple w-lg h-lg">
+            {uploadImage ? (
+              <Image
+                src={uploadImage}
+                priority={true}
+                alt="avatar"
+                width={100}
+                height={100}
+              />
+            ) : null}
+          </div>
+          <input
+            type="file"
+            accept="image/jpg,image/png,image/jpeg,image/gif"
+            name="profile_img"
+            // className="w-[120px]"
+            onChange={onChange}
+          />
         </div>
         <div className="self-end">
           <Button
--- project/frontend/src/components/sign-in/RegisterUser.tsx
+++ project/frontend/src/components/sign-in/RegisterUser.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import { ApiContext } from '../../app/_internal/provider/ApiContext';
 import { Button } from '../common/Button';
 
@@ -17,27 +17,45 @@ export default function RegisterUser({
 }: RegisterUserProps) {
   const { api } = useContext(ApiContext);
   const router = useRouter();
+  const [formData, setFormData] = useState(new FormData());
 
-  const handleSubmitClick = useCallback(() => {
-    if (!nickname || !imageUrl) {
+  const handleFileChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files?.[0];
+      if (file) {
+        const newFormData = new FormData();
+        newFormData.append('file', file);
+        setFormData(newFormData);
+      }
+    },
+    [],
+  );
+
+  const handleSubmitClick = useCallback(async () => {
+    if (!nickname || !formData) {
       alert('// TODO: 닉네임이나 아바타 설정 안 했을 때 에러 메시지 좀 예쁘게');
       return;
     }
-    (async () => {
-      const result = await api.authControllerRegister({
-        nickname,
-        imageUrl,
-      });
+    formData.append('nickname', nickname);
+    console.log(formData);
+    try {
+      const result = await api.authControllerRegister(formData);
       if (!result.ok) {
         console.error({ result });
         alert('// TODO: 뭔가 좀 잘못 됐을 때 에러 메시지 좀 예쁘게');
+      } else {
+        router.push('/friend');
       }
-      router.push('/friend');
-    })();
-  }, [api, imageUrl, nickname, router]);
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+  }, [api, formData, nickname, router, imageUrl]);
 
   return (
-    <div className="w-xl h-xl bg-light-background border-2 border-dark-purple rounded-lg flex flex-col gap-xl justify-center items-start p-xl">
+    <form
+      className="w-xl h-xl bg-light-background border-2 border-dark-purple rounded-lg flex flex-col gap-xl justify-center items-start p-xl"
+      encType="multipart/form-data"
+    >
       <p className="font-semibold text-h3">선택한 닉네임: {nickname}</p>
       <div className="flex flex-row items-center">
         <p className="font-semibold text-h3">선택한 아바타: </p>
@@ -52,6 +70,6 @@ export default function RegisterUser({
       <div className="self-end">
         <Button onClick={handleSubmitClick}>가입하기!</Button>
       </div>
-    </div>
+    </form>
   );
 }
