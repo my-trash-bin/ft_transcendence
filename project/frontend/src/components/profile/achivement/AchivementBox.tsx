3fadc72a0d28d462a66fc4e553c469b44fa3f1fe
christie26 <yoonseo9800@gmail.com>
2023-12-04 16:17:03 +0900

    connect achievement api

--- project/frontend/src/components/profile/achivement/AchivementBox.tsx
+++ project/frontend/src/components/profile/achivement/AchivementBox.tsx
@@ -1,11 +1,22 @@
 import { Title } from '@/components/common/Title';
 import { Badge } from './Badge';
-import { mockAchivementData } from './mockAchivementData';
+import { useContext, useCallback } from 'react';
 import { Button } from '@/components/common/Button';
 import { useRouter } from 'next/navigation';
+import { useQuery } from 'react-query';
+import { ApiContext } from '@/app/_internal/provider/ApiContext';
+import { Loading } from '@/components/common/Loading';
 
 export function AchivementBox() {
   const router = useRouter();
+  const { api } = useContext(ApiContext);
+  const { isLoading, data } = useQuery(
+    ['fetchAchievement1'],
+    useCallback(
+      async () => (await api.achievementControllerFindAll()).data,
+      [api],
+    ),
+  );
 
   return (
     <div className="w-[435px] h-[420px] bg-light-background rounded-lg relative">
@@ -14,20 +25,33 @@ export function AchivementBox() {
         <Button onClick={() => router.push('/profile/achivement')} size={'big'}>
           더보기
         </Button>
-        <div className="grid grid-cols-3 h-[500px] w-[90%] mt-xl gap-md px-sm overflow-y-scroll">
-          {mockAchivementData.map((badge) =>
-            badge.isMine ? (
-              <Badge
-                key={badge.key}
-                size="small"
-                nameContent={badge.name}
-                commentContent={null}
-                imageURL={badge.imageURL}
-              />
-            ) : null,
-          )}
-        </div>
+        <div className="w-[100%] h-[100%] pt-xl ">{render()}</div>
       </div>
     </div>
   );
+
+  function render() {
+    if (isLoading) return <Loading width={300} />;
+
+    const filteredData = data?.filter((badge) => badge.isMine);
+
+    console.log(filteredData);
+    if (filteredData?.length === 0 || !filteredData)
+      return (
+        <p className="self-center text-center">아직 획득한 업적이 없습니다. </p>
+      );
+    return (
+      <div className="grid grid-cols-3 h-[90%] w-[90%] gap-md px-sm overflow-y-scroll">
+        {filteredData.map((badge) => (
+          <Badge
+            key={badge.id}
+            size="small"
+            nameContent={badge.title}
+            commentContent={null}
+            imageURL={badge.imageUrl}
+          />
+        ))}
+      </div>
+    );
+  }
 }
