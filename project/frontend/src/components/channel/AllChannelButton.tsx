2c65ad6a823a7e66211d65ef05b3a9492eca6b69
wken5577 <wken5577@hanmail.net>
2023-12-06 10:18:35 +0900

    [Feat] finish chat (#56)

---
+++ project/frontend/src/components/channel/AllChannelButton.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+
+export function AllChannelButton({
+  id,
+  channelName,
+  now,
+  max,
+  type,
+  passwordModalOpen,
+  participateModalOpen,
+  setSelectedChannel,
+}: Readonly<{
+  id: string;
+  channelName: string;
+  now: number;
+  max: number;
+  type: string;
+  passwordModalOpen: (isModalOpen: boolean) => void;
+  participateModalOpen: (isModalOpen: boolean) => void;
+  setSelectedChannel: (channelId: string) => void;
+}>) {
+  const state = now + '/' + max;
+
+  const addUserToChannel = () => {
+    setSelectedChannel(id);
+    if (type === 'public') {
+      participateModalOpen(true);
+    } else if (type === 'protected') {
+      passwordModalOpen(true);
+      // if(password)
+      //   getSocket().emit('join', { channelId: id, password: password });
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={addUserToChannel}
+        className="w-[320px] h-[60px] pl-[15px] pr-[12px]
+     bg-white border border-default rounded-md mb-[10px] shrink-0 flex-row flex items-center justify-between"
+      >
+        <p className="self-center text-dark-gray">{channelName}</p>
+        <div className="flex items-center">
+          {type === 'protected' ? (
+            <Image
+              src="/icon/lock.svg"
+              width={25}
+              height={25}
+              alt="lock icon"
+            />
+          ) : (
+            ''
+          )}
+          <p className="text-dark-gray ml-[10px]">{state}</p>
+        </div>
+      </button>
+    </>
+  );
+}
---
+++ project/frontend/src/components/channel/MyChannelButton.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { formatAMPM } from '../dm/utils/FromatAmPm';
+export function MyChannelButton({
+  channelName,
+  messageShortcut,
+  date,
+  max,
+  now,
+  id,
+}: Readonly<{
+  channelName: string;
+  messageShortcut?: string;
+  date?: Date;
+  max: number;
+  now: number;
+  id: string;
+}>) {
+  const dateView = date ? formatAMPM(date) : '';
+  const state = now + '/' + max;
+  return (
+    <Link href={`/channel/${id}`}>
+      <button className="w-[320px] h-[90px] bg-white border border-default rounded-md shrink-0 p-sm pl-md">
+        <div className="flex flex-col h-[100%] justify-between">
+          <div className="flex flex-row justify-between items-center">
+            <p className="text-[22px] text-left">{channelName}</p>
+            {date ? <p className="text-[12px] text-right ">{dateView}</p> : ''}
+          </div>
+          <div className="flex flex-row justify-between items-center">
+            {messageShortcut ? (
+              <p className="text-[13px] text-left text-slate-500">
+                {messageShortcut}
+              </p>
+            ) : (
+              <p className="text-[13px] text-left text-slate-500">
+                대화내용이 없습니다.
+              </p>
+            )}
+
+            <p className="text-[12px] text-right">{state}</p>
+          </div>
+        </div>
+      </button>
+    </Link>
+  );
+}
