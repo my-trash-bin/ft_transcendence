c27c29890fc1d732d0e21b98094e1a3415092ba1
christie26 <yoonseo9800@gmail.com>
2023-12-07 18:46:27 +0900

    height layout min-h

--- project/frontend/src/components/common/MainLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-
-export function MainLayout({ children }: { readonly children: ReactNode }) {
-  return (
-    <div className="w-[900px] h-[750px] m-auto font-sejong">{children}</div>
-  );
-}
