ddd39f38ce2265f3c49d3ac8c022b8c87b46784e
맹주영 <mjy9088@naver.com>
2023-12-09 12:49:22 +0900

    remove env-validator

--- docker/env-validator/validate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-(cd "$(dirname "$0")/../.." && docker build -t ft_transcendence-env-validator -f docker/env-validator/Dockerfile .)
-
-docker run -i ft_transcendence-env-validator < "$1"
