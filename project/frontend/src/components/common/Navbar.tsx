8a050c1509ed7c866459d046d9333045455fece5
minju <peppertones0123@gmail.com>
2023-12-09 14:09:42 +0900

    [feat] game invitation

--- project/frontend/src/components/common/Navbar.tsx
+++ project/frontend/src/components/common/Navbar.tsx
@@ -1,8 +1,39 @@
+'use client';
 import Logo from './Logo';
 import NavIcon from './NavIcon';
 import { Notification } from '../notification/Notification';
+import { getGameSocket } from '../pong/gameSocket';
+import { inviteStore } from '../pong/Update';
+import { useEffect } from 'react';
 
 const Navbar = () => {
+  const socket = getGameSocket();
+  const { setInvite } = inviteStore();
+
+  const handleInvited = (inviterId: string, mode: string) => {
+    setInvite(inviterId, mode);
+    // noti
+    console.log('invited', inviterId, mode);
+  };
+
+  useEffect(() => {
+    socket.on('invitedNormalMatch', handleInvited);
+    socket.on('invitedItemMatch', handleInvited);
+    return () => {
+      socket.off('invitedNormalMatch', handleInvited);
+      socket.off('invitedItemMatch', handleInvited);
+    };
+  }, [socket, handleInvited]);
+
+  useEffect(() => {
+    socket.on('friendIsOffline', (friendId: string) => {
+      console.log('friendIsOffline', friendId);
+    });
+    return () => {
+      socket.off('friendIsOffline');
+    };
+  });
+
   return (
     <nav
       className={
