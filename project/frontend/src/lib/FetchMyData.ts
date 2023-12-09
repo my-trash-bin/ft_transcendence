bf8e5caacb6ad7f99005c88c8912e10e3340ee3f
맹주영 <mjy9088@naver.com>
2023-12-09 18:55:06 +0900

    remove incorrect usage of `new Api()`

--- project/frontend/src/lib/FetchMyData.ts
+++
@@ -1,7 +0,0 @@
-import { Api } from '@/api/api';
-
-export async function fetchMyData(setIsLoading?: (isLoading: boolean) => void) {
-  const data = await new Api().api.usersControllerMyProfile();
-  localStorage.setItem('me', JSON.stringify(data.data));
-  if (setIsLoading) setIsLoading(false);
-}
