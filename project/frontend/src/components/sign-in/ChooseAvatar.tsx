fb7e6b65abcdadb72358ec02facc8f05a54ad1cb
christie26 <yoonseo9800@gmail.com>
2023-12-11 14:40:25 +0900

    error handle during sign-in #174

--- project/frontend/src/components/sign-in/ChooseAvatar.tsx
+++ project/frontend/src/components/sign-in/ChooseAvatar.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import { ApiContext } from '@/app/_internal/provider/ApiContext';
 import { avatarToUrl } from '@/app/_internal/util/avatarToUrl';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Button } from '../common/Button';
 import { SelectAvatar } from './SelectAvatar';
 
@@ -17,6 +18,8 @@ export default function ChooseAvatar({
 }: ChooseAvatarProps) {
   const [selectedAvatar, setSelectedAvatar] = useState<string>(() => '');
   const [uploadImage, setUploadImage] = useState<any>(null);
+  const { api } = useContext(ApiContext);
+  const [isError, setIsError] = useState(false);
 
   const handleFileChange = async (event: any) => {
     const selectedFile = event.target.files[0];
@@ -44,10 +47,9 @@ export default function ChooseAvatar({
     }
   };
 
-  async function callApi(selectedFile: any) {
+  const callApi = async function (selectedFile: any) {
     try {
-      const response = await fetch('/api/v1/avatar/upload', {
-        method: 'POST',
+      const response = await api.avatarControllerUploadFile({
         body: selectedFile,
         headers: {
           'Content-Type': selectedFile.type,
@@ -58,61 +60,76 @@ export default function ChooseAvatar({
         const responseData = await response.json();
         setSelectedAvatar(responseData.filePath);
       } else {
-        console.error('Error uploading file...');
+        setIsError(true);
+        console.error('Error uploading file', response);
       }
     } catch (error) {
+      setIsError(true);
       console.error('Error uploading file:', error);
     }
-  }
+  };
+
   return (
     <div className="w-2xl h-[400px] bg-light-background rounded-lg flex flex-col justify-center items-center px-2xl">
       <h2 className="font-bold mb-lg">2. 사용하실 아바타를 선택하세요.</h2>
-      <div className="grid grid-cols-3 grid-flow-row gap-xl mb-lg">
-        {avatars.map((avatar) => (
-          <SelectAvatar
-            key={avatar}
-            imageUrl={avatarToUrl(avatar)}
-            isSelected={selectedAvatar === avatar}
-            onClick={() => setSelectedAvatar(avatar)}
-          />
-        ))}
-        <div
-          className={`${
-            uploadImage
-              ? 'border-dark-purple'
-              : 'border-default hover:border-dark-gray hover:bg-light-background'
-          }  w-lg h-lg border-3 inline-block overflow-x-hidden overflow-y-hidden`}
-        >
-          {uploadImage ? (
-            <Image
-              src={uploadImage}
-              priority={true}
-              alt="avatar"
-              width={100}
-              height={100}
-            />
-          ) : null}
-        </div>
-      </div>
-      <input
-        type="file"
-        accept="image/jpg,image/png,image/jpeg,image/gif"
-        name="profile_img"
-        className="text-md ml-[120px]"
-        onChange={handleFileChange}
-      />
-      <div className="self-end">
-        <Button
-          onClick={() => {
-            if (typeof selectedAvatar === 'string') {
-              onChooseClick(selectedAvatar);
-            }
-          }}
-          disabled={!selectedAvatar}
-        >
-          다음으로
-        </Button>
-      </div>
+      {render()}
     </div>
   );
+
+  function render() {
+    if (isError) {
+      return <>알 수 없는 에러입니다.</>;
+    } else {
+      return (
+        <div className="flex flex-col justify-center items-center">
+          <div className="grid grid-cols-3 grid-flow-row gap-xl mb-lg">
+            {avatars.map((avatar) => (
+              <SelectAvatar
+                key={avatar}
+                imageUrl={avatarToUrl(avatar)}
+                isSelected={selectedAvatar === avatar}
+                onClick={() => setSelectedAvatar(avatar)}
+              />
+            ))}
+            <div
+              className={`${
+                uploadImage
+                  ? 'border-dark-purple'
+                  : 'border-default hover:border-dark-gray hover:bg-light-background'
+              }  w-lg h-lg border-3 inline-block overflow-x-hidden overflow-y-hidden`}
+            >
+              {uploadImage ? (
+                <Image
+                  src={uploadImage}
+                  priority={true}
+                  alt="avatar"
+                  width={100}
+                  height={100}
+                />
+              ) : null}
+            </div>
+          </div>
+          <input
+            type="file"
+            accept="image/jpg,image/png,image/jpeg,image/gif"
+            name="profile_img"
+            className="text-md ml-[120px]"
+            onChange={handleFileChange}
+          />
+          <div className="self-end">
+            <Button
+              onClick={() => {
+                if (typeof selectedAvatar === 'string') {
+                  onChooseClick(selectedAvatar);
+                }
+              }}
+              disabled={!selectedAvatar}
+            >
+              다음으로
+            </Button>
+          </div>
+        </div>
+      );
+    }
+  }
 }
