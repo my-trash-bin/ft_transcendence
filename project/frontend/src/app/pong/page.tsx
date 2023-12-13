576e640bb373fcd69d325f5c325f89c4ea230402
christie26 <yoonseo9800@gmail.com>
2023-12-13 18:57:45 +0900

    change function name

--- project/frontend/src/app/pong/page.tsx
+++ project/frontend/src/app/pong/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 import withAuth from '@/components/auth/withAuth';
-import Pong from '../../components/pong/Board';
+import Board from '../../components/pong/Board';
 
-function FriendHome() {
+function PongHome() {
   return (
     <div className="flex flex-row h-[100%] w-[100%]">
       <div className="flex flex-row w-[100%]">
         <div className={`flex flex-col items-center max-w-4xl mx-auto`}>
-          <Pong />
+          <Board />
         </div>
       </div>
     </div>
   );
 }
 
-export default withAuth(FriendHome, 'friend');
+export default withAuth(PongHome, 'friend');
