f466557ff1ccecb348537616735aa9d281ee6f0d
christie26 <yoonseo9800@gmail.com>
2023-12-13 15:27:10 +0900

    fix avatar ratio

--- project/frontend/src/components/dm/dm-user/DmUser.tsx
+++ project/frontend/src/components/dm/dm-user/DmUser.tsx
@@ -1,7 +1,7 @@
+import { avatarToUrl } from '@/app/_internal/util/avatarToUrl';
 import Image from 'next/image';
 import Link from 'next/link';
 import { formatAMPM } from '../utils/FromatAmPm';
-import { avatarToUrl } from '@/app/_internal/util/avatarToUrl';
 export function DmUser({
   imageUri,
   nickname,
@@ -17,14 +17,16 @@ export function DmUser({
   return (
     <Link href={`/dm/${nickname}`}>
       <button className="w-[320px] h-[90px] bg-white border border-default rounded-md shrink-0 p-sm">
-        <div className="flex flex-row gap-md h-[100%]">
-          <Image
-            alt="user image short cut dm"
-            src={avatarToUrl(imageUri)}
-            width="40"
-            height="40"
-          />
-          <div className="flex flex-col h-[100%] w-[100%] justify-between">
+        <div className="flex flex-row gap-lg h-[100%] justify-between items-center">
+          <div className="w-[50px] h-[50px]">
+            <Image
+              alt="user image short cut dm"
+              src={avatarToUrl(imageUri)}
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="flex flex-col h-[100%] w-[230px] justify-between">
             <div className="flex flex-row justify-between items-center">
               <p className="text-[22px] text-left  font-semibold">{nickname}</p>
               <p className="text-[12px] text-right ">{dateView}</p>
--- project/frontend/src/components/dm/message/UserInfo.tsx
+++ project/frontend/src/components/dm/message/UserInfo.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 export function UserInfo({
   imageUri,
   username,
-  onActive,
   targetId,
 }: Readonly<{
   imageUri: any;
@@ -14,14 +13,15 @@ export function UserInfo({
   targetId: string;
 }>) {
   return (
-    <div className="h-[80px] w-[95%] border-b border-default flex flex-row">
-      <Image
-        alt="userImage"
-        src={avatarToUrl(imageUri)}
-        width={35}
-        height={40}
-        className="ml-[20px]"
-      />
+    <div className="h-[80px] w-[95%] border-b border-default flex flex-row ml-[20px] items-center">
+      <div className="w-[50px] h-[50px]">
+        <Image
+          alt="userImage"
+          src={avatarToUrl(imageUri)}
+          width={50}
+          height={50}
+        />
+      </div>
       <div className="ml-[20px] self-center">
         <p className="top-[10px] left-[100px] text-[22px]">{username}</p>
         <LiveStatus targetId={targetId} />
