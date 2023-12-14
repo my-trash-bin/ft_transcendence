8fb62b22f8b544db2aa7a62dfded4331a884a81f
christie26 <yoonseo9800@gmail.com>
2023-12-14 17:14:56 +0900

    add toast to unique check

--- project/frontend/src/components/sign-in/ChooseNickname.tsx
+++ project/frontend/src/components/sign-in/ChooseNickname.tsx
@@ -2,7 +2,6 @@
 
 import { UniqueCheckResponse } from '@/api/api';
 import { useContext, useState } from 'react';
-import toast from 'react-hot-toast';
 import { ApiContext } from '../../app/_internal/provider/ApiContext';
 import { Button } from '../common/Button';
 
@@ -18,6 +17,8 @@ export default function ChooseNickname({
   const [isValid, setIsValid] = useState(false);
   const { api } = useContext(ApiContext);
   const [isError, setIsError] = useState(false);
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [showFailToast, setShowFailToast] = useState(false);
 
   function isNicknameValid(nickname: string): boolean {
     const nicknameRegex = /^[a-zA-Z0-9\-_]{6,12}$/;
@@ -30,7 +31,16 @@ export default function ChooseNickname({
         nickname: nickname,
       });
       const result: UniqueCheckResponse = response.data;
-      setIsUnique(result.isUnique);
+      if (result.isUnique !== isUnique) {
+        if (result.isUnique === true) {
+          setShowSuccessToast(true);
+          setTimeout(() => setShowSuccessToast(false), 2000);
+        } else if (result.isUnique === false) {
+          setShowFailToast(true);
+          setTimeout(() => setShowFailToast(false), 2000);
+        }
+        setIsUnique(result.isUnique);
+      }
     } catch (error: any) {
       setIsError(true);
       console.error('Error during unique check:', error);
@@ -44,29 +54,20 @@ export default function ChooseNickname({
     setIsValid(isNicknameValid(newNickname));
   }
 
-  function showUnique(unique: boolean | undefined): void {
-    if (unique !== isUnique) {
-      setIsUnique(unique);
-
-      if (unique === true) {
-        toast.success('Nickname is unique!');
-      } else if (unique === false) {
-        toast.error('Nickname is not unique!');
-      }
-    }
-  }
-
   const handleButtonClick = async () => {
     if (isValid) {
       await checkUnique();
-      showUnique(isUnique);
     }
   };
+  const css =
+    'absolute right-0 bottom-0 h-[50px] p-sm transform flex justify-center items-center bg-default border-2 border-dark-purple text-dark-purple rounded-md z-50 text-lg';
 
   return (
-    <div className="w-2xl h-[400px] bg-light-background rounded-lg flex flex-col justify-center items-center">
-      <h2 className=" mb-xl">1. 사용하실 닉네임을 입력하세요.</h2>
-      {render()}
+    <div className="w-[500px] h-[500px]">
+      <div className="w-2xl h-[400px] bg-light-background rounded-lg flex flex-col justify-center items-center">
+        <h2 className=" mb-xl">1. 사용하실 닉네임을 입력하세요.</h2>
+        {render()}
+      </div>
     </div>
   );
   function render() {
@@ -75,7 +76,7 @@ export default function ChooseNickname({
     } else {
       return (
         <div className="flex flex-col">
-          <div className="flex flex-row">
+          <div className="flex flex-row relative">
             <div className="flex flex-col">
               <input
                 type="text"
@@ -88,6 +89,14 @@ export default function ChooseNickname({
                 닉네임은 6-12자 이어야 합니다. <br />
                 영문, 숫자, -, _ 중에서 사용하세요.{' '}
               </p>
+              {showSuccessToast && (
+                <div className={css}>사용 가능한 아이디 입니다.</div>
+              )}
+              {showFailToast && (
+                <div className={css}>
+                  이미 사용중인 아이디입니다. 다른 아이디를 선택하세요
+                </div>
+              )}
             </div>
             <Button onClick={handleButtonClick} disabled={!isValid}>
               중복확인
