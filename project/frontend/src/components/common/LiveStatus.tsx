81ddd9f6ada345fb2e80164890594ee411744bfc
jhj9109 <jhj91_09@naver.com>
2023-12-16 17:50:45 +0900

    상태메시지 롤백 to 'ongame'

--- project/frontend/src/components/common/LiveStatus.tsx
+++ project/frontend/src/components/common/LiveStatus.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 export enum EnumUserStatus {
   ONLINE = 'online',
   OFFLINE = 'offline',
-  ONGAME = 'on game',
+  ONGAME = 'ongame',
 }
 
 const USER_STATUS_EVENT_NAME = 'userStatus';
